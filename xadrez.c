2690b3b21a70983281881a262a9bffab3e067c70
lc-dev258 <lucas.sant2802@gmail.com>
2025-03-29 15:23:54 +0000

    Xadrezz.c

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int main(){
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+//variaveis para as peças
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+int rainha = 0;
+int torre = 0;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+//condição para o looping dos movimentos
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+printf("\n---------- RAINHA ----------\n");
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+while(rainha <= 7)
+    {
+    printf("Direita\n");
+    rainha++;
+}
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+printf("\n---------- BISPO ----------\n");
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+for(int bispo = 0; bispo < 7; bispo++)
+    {
+    printf("Diagonal pra cima\n");
+}
 
-    return 0;
+for(int bispo = 0; bispo < 1; bispo++)
+    {
+    printf("Direita\n");
+    }
+
+    printf("\n---------- TORRE ----------\n");
+
+while(torre <= 4)
+    {
+    printf("Direita\n");
+    torre++;
+}
+
+return 0;
+
 }
