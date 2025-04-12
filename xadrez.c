8edcaa05eb70dc6372c77919851ee4e46cdb2548
lc-dev258 <lucas.sant2802@gmail.com>
2025-04-12 14:05:44 +0000

    Upgrade final do desafio de xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,56 +1,50 @@
 #include <stdio.h>
 
+
+void moverRainha(int rainha){                           //Recursividade para fazer os movimentos da rainha, do bispo, e da torre
+if(rainha < 7){
+    printf ("Esquerda\n");
+    moverRainha(rainha + 1);
+}
+}
+
+void moverBispo(int bispo){
+if(bispo < 7){
+    printf("Diagonal pra cima\n");
+    moverBispo(bispo + 1);
+}
+}
+
+void moverTorre(int torre){
+if(torre <= 4){
+    printf ("Direita\n");
+    moverTorre(torre + 1);
+}
+}
 int main(){
-
-//variaveis para as peças
-
-int rainha = 0;
-int torre = 0;
-
-//condição para o looping dos movimentos
 
 printf("\n---------- RAINHA ----------\n");
 
-while(rainha <= 7) //rainha se move 7 vezes para a direita
-    {
-    printf("Direita\n");
-    rainha++;
-}
+moverRainha(0);
+
+printf("\n---------- TORRE ----------\n");
+
+moverTorre(0);
 
 printf("\n---------- BISPO ----------\n");
 
-for(int bispo = 0; bispo < 7; bispo++) //bispo se move até 6 vezes na diagonal pra cima
-    {
-    printf("Diagonal pra cima\n");
+moverBispo(0);
+
+
+printf("\n---------- CAVALO ----------\n");
+
+for(int i = 0; i < 2; i++){
+        printf("Baixo\n");                      //loop com for para fazer os movimentos do cavalo
 }
-
-for(int bispo = 0; bispo < 1; bispo++)
-    {
-    printf("Direita\n");
-    }
-
-    printf("\n---------- TORRE ----------\n");
-
-while(torre <= 4) //torre se move 4 vezes em linha reta para a direita
-    {
-    printf("Direita\n");
-    torre++;
-}
-
-     printf("\n---------- CAVALO ----------\n");
-
-
-    // Dois passos para baixo
-    for (int i = 0; i < 2; i++) {
-        printf("Baixo\n");
-    }
-
-    // Um passo para a esquerda
-    int esquerda = 0;
-    while (esquerda < 1) {
+        for(int j = 0; j < 1; j++){
         printf("Esquerda\n");
-        esquerda++;
-    }
-return 0;
+        }
 
+
+return 0;
 }
